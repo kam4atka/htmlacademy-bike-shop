02774badab515b3432e154cce1d9054e70246161
kamcha7ka <sledsib@ya.ru>
2020-02-04 11:30:45 +0300

    1.00 - [root] Добавит минимальный набор файлов для сборки проекта
      root:
        - файлы конфигурации и скрипты неободиые для сборки проекта
      source
        - минимальный набор файлов стилизации
        - основные шрифты проекта
        - иконки для блока HEADER

---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+"use strict";
+
+var gulp = require("gulp");
+
+var server = require("browser-sync").create();
+var rename = require("gulp-rename");
+var del = require("del");
+var concat = require("gulp-concat");
+
+var plumber = require("gulp-plumber");
+
+var sass = require("gulp-sass");
+var sourcemap = require("gulp-sourcemaps");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore")
+
+var uglify = require("gulp-uglify");
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/js/common/*.js", gulp.series("js-common", "refresh"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+
+    .pipe(gulp.dest("source/img"));
+
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite_auto.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("js-common", function () {
+  return gulp.src("source/js/common/*.js")
+    .pipe(uglify())
+    .pipe(rename("main.js"))
+    .pipe(gulp.dest("build/js"));
+});
+
+// gulp.task("js-vendor", function () {
+//   return gulp.src("source/js/vendor/*.js")
+//     .pipe(concat("vendor.js"))
+//     .pipe(gulp.dest("build/js"));
+// });
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source//*.ico"
+    ], {
+      base: "source"
+    })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html", "js-common"));
+gulp.task("start", gulp.series("build", "server"));
