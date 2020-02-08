8913bbfa81f59250741434999ccd96efa3dfd2c3
kamcha7ka <sledsib@ya.ru>
2020-02-08 11:03:55 +0300

    2.07 - [source] Добавит исправления в код блока HEADER
      index.html:
        - добавляет разметку оставшихся блоков главной страницы сайта
      sass:
        - добавляет правила для правильного позиционирования блока HEADER.
        Результат: позиция static блока HEADER при "javascript NO";
        - выносит общие правила стилизации блока SERVICES в отдельный файл;
        - добавляет файл стилизации main.scss
      js:
        - исправляет поведение меню. Результат - срабатывание preventDefault
        происходит при соответствующем условии;

--- source/js/common/main.js
+++ source/js/common/main.js
@@ -8,14 +8,15 @@
     nojs.classList.remove('nojs');
 
     var navToggleHandler = function (ntEvt) {
-      ntEvt.preventDefault();
       if (ntEvt.target.classList.contains('nav-main__head-toggle_open')) {
+        ntEvt.preventDefault();
         ntEvt.currentTarget.classList.remove('nav-main_close');
         ntEvt.target.classList.remove('nav-main__head-toggle_open');
         ntEvt.target.classList.add('nav-main__head-toggle_close');
         return;
       }
       if (ntEvt.target.classList.contains('nav-main__head-toggle_close')) {
+        ntEvt.preventDefault();
         ntEvt.currentTarget.classList.add('nav-main_close');
         ntEvt.target.classList.add('nav-main__head-toggle_open');
         ntEvt.target.classList.remove('nav-main__head-toggle_close');
