fba4b3d92f23e143dd5a114c2b4e1d3a242f18d0
kamcha7ka <sledsib@ya.ru>
2020-02-12 14:25:42 +0300

    3.05 - [source] Добавит код для страницы каталога товаров
      catalog.html:
        - добавляет код разметки для блоков сортировки, фильтрации, списка
        товаров и пагинации;
      sass:
        - добавляет правила стилизации для блоков сортировки, фильтрации, списка
        товаров и пагинации;
      js:
        - добавляет логику работы блока фильтрации на мобильных устройствах.

--- source/js/common/main.js
+++ source/js/common/main.js
@@ -7,12 +7,22 @@
     var nojs = document.querySelector('.nojs');
     nojs.classList.remove('nojs');
 
+    var bodyScroll = function (action) {
+      if (action === 'off') {
+        document.body.style.overflow = 'hidden';
+      }
+      if (action === 'on') {
+        document.body.style.overflow = 'scroll';
+      }
+    };
+
     var navToggleHandler = function (ntEvt) {
       if (ntEvt.target.classList.contains('nav-main__head-toggle_open')) {
         ntEvt.preventDefault();
         ntEvt.currentTarget.classList.remove('nav-main_close');
         ntEvt.target.classList.remove('nav-main__head-toggle_open');
         ntEvt.target.classList.add('nav-main__head-toggle_close');
+        bodyScroll('off');
         return;
       }
       if (ntEvt.target.classList.contains('nav-main__head-toggle_close')) {
@@ -20,13 +30,38 @@
         ntEvt.currentTarget.classList.add('nav-main_close');
         ntEvt.target.classList.add('nav-main__head-toggle_open');
         ntEvt.target.classList.remove('nav-main__head-toggle_close');
+        bodyScroll('on');
         return;
       }
+    };
+
+    var filterCloseHandler = function (fcEvt) {
+      var root = fcEvt.currentTarget;
+      if (fcEvt.target.classList.contains('filter__close')) {
+        if (!root.classList.contains('filter_mobile')) {
+          root.classList.add('filter_mobile');
+          bodyScroll('on');
+          root.removeEventListener('click', filterCloseHandler);
+        }
+      }
+    };
+
+    var filterOpenHandler = function (ftEvt) {
+      var filter = document.querySelector('.filter');
+      if (filter.classList.contains('filter_mobile')) {
+        ftEvt.preventDefault();
+        filter.classList.remove('filter_mobile');
+        bodyScroll('off');
+        filter.addEventListener('click', filterCloseHandler);
+      }
+
     };
 
     if (document.body.offsetWidth <= TABLET_SIZE) {
       var nav = document.querySelector('.nav-main');
       nav.addEventListener('click', navToggleHandler);
+      var filterOpen = document.querySelector('.sort__toggle');
+      filterOpen.addEventListener('click', filterOpenHandler);
     }
   };
   window.addEventListener('load', initPage);
